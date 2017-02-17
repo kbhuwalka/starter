6a96182ec5f0ec3111969eedfaf6b05fa8a474b0
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2017-02-17 20:06:40 +0530

    fix: Removed unecessary files
    Removed the files that were used for testing tasks, these are not
    required in the template.

--- src/dist/js/admin_custom.js
+++
@@ -1,1 +0,0 @@
-!function(o){o(function(){o(".button-collapse").sideNav(),o(".modal").modal(),o(".collapsible").collapsible({accordion:!1,onOpen:function(o){o.find(".collapsible-header #action-icons").show()},onClose:function(o){o.find(".collapsible-header #action-icons").hide()}})})}(jQuery);
